7f2ea3efd2ecec10906d8d5b594257a2e640b475
AlNat <aleksnatarov@gmail.com>
2016-12-01 20:54:05 +0300

    Spaces updates

--- LSH/src/com/LSH/client/LSHServiceAsync.java
+++ LSH/src/com/LSH/client/LSHServiceAsync.java
@@ -5,5 +5,4 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 public interface LSHServiceAsync {
     void getSimpleShort(String msg, AsyncCallback<String> async);
     void getComplexShort(Message msg, AsyncCallback<String> async);
-
 }
--- LSH/src/com/LSH/server/LSHServiceImpl.java
+++ LSH/src/com/LSH/server/LSHServiceImpl.java
@@ -4,24 +4,22 @@ import com.LSH.client.Message;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.LSH.client.LSHService;
 
+// TODO Комментари + JavaDoc
+// TODO имплемантация
 public class LSHServiceImpl extends RemoteServiceServlet implements LSHService {
 
     public String getSimpleShort(String msg) {
 
         /*
         String answer = Shortner.GetShort(Integer.parseInt(msg));
-
         answer = Normalizer.Normalize(answer);
-
         return answer;*/
 
         return Shortner.GetShort(Integer.parseInt(msg));
     }
 
     public String getComplexShort(Message msg) {
-        String status = "OK";
-
-        return status;
+        return Shortner.GetShort(Integer.parseInt(msg.getOriginalLink()));
     }
 
 
