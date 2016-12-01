5fc7b304dd7ab2098f791bd01e6dcc8f0c002740
AlNat <aleksnatarov@gmail.com>
2016-12-01 20:53:53 +0300

    Massage datatype complete

--- LSH/src/com/LSH/client/Message.java
+++ LSH/src/com/LSH/client/Message.java
@@ -1,14 +1,64 @@
 package com.LSH.client;
 
 import java.io.Serializable;
-import java.sql.Timestamp;
 
 /**
  * Created by @author AlNat on 01.12.2016.
  * Licensed by Apache License, Version 2.0
+ *
+ * Класс обертка для сообщения между клиентом и сервером
+ * для передачи данных об управляемом сворачивании ссылки
  */
 public class Message implements Serializable {
-    String link;
-    Timestamp timestamp;
-    Integer maxVisits;
+
+    /* Геттеры и сеттеры */
+
+    public String getOriginalLink() {
+        return originalLink;
+    }
+
+    public String getShortLink() {
+        return shortLink;
+    }
+
+    void setShortLink(String shortLink) {
+        if (!shortLink.isEmpty()) {
+            this.shortLink = shortLink;
+        }
+    }
+
+    public String getTtl() {
+        return ttl;
+    }
+
+    void setTtl(String ttl) {
+        this.ttl = ttl;
+    }
+
+    public Integer getMaxVisits() {
+        return maxVisits;
+    }
+
+    void setMaxVisits(Integer maxVisits) {
+        this.maxVisits = maxVisits;
+    }
+
+    private String originalLink;
+    private String shortLink;
+    private String ttl;
+    private Integer maxVisits;
+
+    Message () {
+        originalLink = "NONE";
+    }
+
+    Message(String originalLink) {
+        this.originalLink = originalLink;
+        shortLink = "NONE"; // Кодовое слово
+        ttl = "1 week"; // Неделю по дефолту
+        maxVisits = 0;
+    }
+
+
+
 }
