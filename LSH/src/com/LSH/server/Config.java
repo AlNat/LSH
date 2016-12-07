b4dc985f599884632d433ff012523afd2adebcd4
AlNat <aleksnatarov@gmail.com>
2016-12-07 18:04:42 +0300

    Create Configger

---
+++ LSH/src/com/LSH/server/Config.java
@@ -0,0 +1,17 @@
+package com.LSH.server;
+
+/**
+ * Created by @author AlNat on 07.12.2016.
+ * Licensed by Apache License, Version 2.0
+ */
+class Config {
+
+    // TODO поля конфигурации
+    // TODO Читатать их из файла + сам файл - JSON/XML
+    // TODO Геттеры/сеттеры на данные
+
+    public void ReadConfig () {
+
+    }
+
+}
