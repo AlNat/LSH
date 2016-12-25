b8354543ce8de8da67afa5d9a614d7617d0074b5
AlNat <aleksnatarov@gmail.com>
2016-12-25 21:36:35 +0300

    Amdin UI complete in a half

--- LSH/src/com/Administration/client/Administration.java
+++ LSH/src/com/Administration/client/Administration.java
@@ -1,24 +1,30 @@
 package com.Administration.client;
 
-import com.google.gwt.cell.client.EditTextCell;
-import com.google.gwt.cell.client.FieldUpdater;
-import com.google.gwt.cell.client.TextCell;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.event.dom.client.*;
+import com.google.gwt.i18n.client.DateTimeFormat;
+
+import com.google.gwt.cell.client.*;
 import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.ColumnSortEvent.ListHandler;
 import com.google.gwt.user.cellview.client.SimplePager;
+
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.*;
+
 import com.google.gwt.view.client.ListDataProvider;
 import com.google.gwt.view.client.ProvidesKey;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
+
 import java.util.Comparator;
+import java.util.Date;
 import java.util.LinkedList;
+
 
 /**
  * Класс, отвечающий за страницу администрированич
@@ -39,7 +45,7 @@ public class Administration implements EntryPoint {
     private ListDataProvider<LinkData> dataProvider;
     private ProvidesKey<LinkData> KEY_PROVIDER;
     private ListHandler<LinkData> sortHandler;
-    private SimplePager pager;
+    private SimplePager pager; // TODO добавить  таблице
 
     /**
      * Основной метод в UI
@@ -70,62 +76,10 @@ public class Administration implements EntryPoint {
         // И писать взаимодействие сервлетов
         // TODO Придумать способ поставить это на другую страницу - Administration.html
 
-
         dialog.show();
         cellTable.setVisible(false);
 
     }
-
-    private void GetData () {
-        AdministrationServiceInterface.App.getInstance().getData(login, new AsyncCallback<LinkedList<LinkData>>() {
-            @Override
-            public void onFailure(Throwable caught) {
-                label.setHTML("<h3>Server error!</h3><br>");
-            }
-
-            @Override
-            public void onSuccess(LinkedList<LinkData> result) {
-                list = result;
-            }
-        });
-
-        // TODO засовывать данные в таблицу
-        initTable();
-        cellTable.setVisible(true);
-
-    }
-
-    /**
-     * Функция, получения MD5 хэша от строки + соль
-     * @param in входная строка
-     * @return хэш строки или null если ошибка
-     */
-    private static String getMD5 (String in) {
-
-        if (in.isEmpty()) {
-            return null;
-        }
-
-        String salt = "SaltSalt";
-        in = salt.toUpperCase() + in + salt.toLowerCase(); // "Солим" пароль
-        // См https://ru.wikipedia.org/wiki/Соль_(криптография)
-
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            byte[] messageDigest = md.digest(in.getBytes());
-            BigInteger number = new BigInteger(1, messageDigest);
-            String hashText = number.toString(16);
-            while (hashText.length() < 32) {
-                hashText = "0" + hashText;
-            }
-            return hashText;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-
-    }
-
 
     /**
      * Диалоговое окно для ввода логина и пароля
@@ -200,9 +154,33 @@ public class Administration implements EntryPoint {
     /**
      *
      */
+    private void GetData () {
+        AdministrationServiceInterface.App.getInstance().getData(login, new AsyncCallback<LinkedList<LinkData>>() {
+            @Override
+            public void onFailure(Throwable caught) {
+                label.setHTML("<h3>Server error!</h3><br>");
+            }
+
+            @Override
+            public void onSuccess(LinkedList<LinkData> result) {
+                list = result;
+            }
+        });
+
+        // TODO засовывать данные в таблицу
+        initTable();
+        cellTable.setVisible(true);
+
+    }
+
+
+    /**
+     *
+     */
     private void initTable() {
 
         // TODO Выделение мышкой
+        // TODO Добавить сортировки
 
         /// Колонка с коротким кодом
 
@@ -246,60 +224,145 @@ public class Administration implements EntryPoint {
 
         originalLinkColumn.setFieldUpdater(new FieldUpdater<LinkData, String>() {
             @Override
-            public void update(int index, LinkData object, String value) {
-                // Called when the user changes the value.
-                String t = value;
-                if ( UpdateData(object, "OriginalLink", value) ) {
+            public void update(int index, LinkData object, String value) { // Called when the user changes the value.
+                if ( UpdateLink(object, value) ) {
+                    // TODO обновлять на сервере
                     object.setLink(value);
                     dataProvider.refresh();
                 } else {
-                    // TODO label ошибка
+                    label.setHTML("Connection error!<br>Can't update data!");
                 }
             }
         });
         cellTable.setColumnWidth(originalLinkColumn, 20, Style.Unit.PCT);
 
-        // TODO Доделать таблицу - createTime, ExpiredData, maxСount, currentCount, password - с кучей звездочек
 
-        /*
-        // DateCell.
-        DateTimeFormat dateFormat = DateTimeFormat.getFormat(PredefinedFormat.DATE_MEDIUM);
-        addColumn(new DateCell(dateFormat), "Date", new GetValue<Date>() {
+
+        Column<LinkData, Date> createTimeColumn = new Column<LinkData, Date>(new DateCell()) {
             @Override
-            public Date getValue(ContactInfo contact) {
-                return contact.getBirthday();
+            public Date getValue(LinkData object) {
+                return object.getCreateDate();
             }
-        }, null);
+        };
+        cellTable.addColumn(createTimeColumn, "Create time");
 
-         // DatePickerCell.
-         addColumn(new DatePickerCell(dateFormat), "DatePicker", new GetValue<Date>() {
+
+        DateTimeFormat dateFormat = DateTimeFormat.getFormat(DateTimeFormat.PredefinedFormat.DATE_MEDIUM);
+        Column<LinkData, Date> expiredDateColumn = new Column<LinkData, Date>(new DatePickerCell(dateFormat)) {
             @Override
-            public Date getValue(ContactInfo contact) {
-                return contact.getBirthday();
+            public Date getValue(LinkData object) {
+                return object.getCreateDate();
             }
-            }, new FieldUpdater<ContactInfo, Date>() {
-                @Override
-                public void update(int index, ContactInfo object, Date value) {
-                    pendingChanges.add(new BirthdayChange(object, value));
+        };
+        cellTable.addColumn(expiredDateColumn, "Expired Date");
+        expiredDateColumn.setFieldUpdater(new FieldUpdater<LinkData, Date>() {
+            @Override
+            public void update(int index, LinkData object, Date value) {
+                if ( UpdateDate(object, value) ) {
+                    // TODO обновлять на сервере
+                    object.setExpiredDate(value);
+                    dataProvider.refresh();
+                } else {
+                    label.setHTML("Connection error!<br>Can't update data!");
                 }
-            });
+            }
+        });
 
-        // NumberCell.
-        Column<ContactInfo, Number> numberColumn =
-            addColumn(new NumberCell(), "Number", new GetValue<Number>() {
-                @Override
-                public Number getValue(ContactInfo contact) {
-                    return contact.getAge();
+
+        Column<LinkData, Number> currentCountColumn = new Column<LinkData, Number>(new NumberCell()) {
+            @Override
+            public Number getValue(LinkData object) {
+                return object.getCurrentCount();
+            }
+        };
+        cellTable.addColumn(currentCountColumn, "Current visits");
+
+        // TODO Поорать на GWT
+        Column<LinkData, String> maxCountColumn = new Column<LinkData, String>(
+                new EditTextCell()) {
+            @Override
+            public String getValue(LinkData object) {
+                return object.getMaxCount().toString(); // Потому что GWT!!!
+            }
+        };
+
+        cellTable.addColumn(originalLinkColumn, "Max Visits");
+
+        originalLinkColumn.setFieldUpdater(new FieldUpdater<LinkData, String>() {
+            @Override
+            public void update(int index, LinkData object, String value) {
+                Integer t = Integer.parseInt(value);
+                if (t == null || t < 0 ) {
+                    label.setHTML("Wrong maximum count!");
                 }
-            }, null);
-        numberColumn.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_LOCALE_END);
-        */
+
+                if ( UpdateLink(object, t) ) {
+                    // TODO обновлять на сервере
+                    object.setMaxCount(t);
+                    dataProvider.refresh();
+                } else {
+                    label.setHTML("Connection error!<br>Can't update data!");
+                }
+            }
+        });
+        cellTable.setColumnWidth(originalLinkColumn, 20, Style.Unit.PCT);
+
+
+
+        Column<LinkData, String> passwordColumn = new Column<LinkData, String>(new EditTextCell()) {
+            @Override
+            public String getValue(LinkData object) {
+                return "*********";
+            }
+        };
+
+        passwordColumn.setFieldUpdater(new FieldUpdater<LinkData, String>() {
+            @Override
+            public void update(int index, LinkData object, String value) {
+
+                // TODO обновлять на сервере
+                if ( UpdatePassword(object, value) ) {
+                    object.setPassword(getMD5(value));
+                    dataProvider.refresh();
+                } else {
+                    label.setHTML("Connection error!<br>Can't update data!");
+                }
+            }
+        });
+        cellTable.setColumnWidth(passwordColumn, 20, Style.Unit.PCT);
+
     }
 
-    private boolean UpdateData (LinkData object, String type, String value) {
-        // TODO обновлять на сервере
-        return false;
+    /**
+     * Функция, получения MD5 хэша от строки + соль
+     * @param in входная строка
+     * @return хэш строки или null если ошибка
+     */
+    private static String getMD5 (String in) {
+
+        if (in.isEmpty()) {
+            return null;
+        }
+
+        String salt = "SaltSalt";
+        in = salt.toUpperCase() + in + salt.toLowerCase(); // "Солим" пароль
+        // См https://ru.wikipedia.org/wiki/Соль_(криптография)
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(in.getBytes());
+            BigInteger number = new BigInteger(1, messageDigest);
+            String hashText = number.toString(16);
+            while (hashText.length() < 32) {
+                hashText = "0" + hashText;
+            }
+            return hashText;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
     }
 
-    
+
 }
